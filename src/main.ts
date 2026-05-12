21b9bd682b73ef93fdb65de7a3f227fd18596f97
Steven <steven@Stevens-MacBook-Air.local>
2026-05-12 13:33:00 +0800

    change host

--- src/main.ts
+++ src/main.ts
@@ -51,7 +51,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://claret.sbs",
-      "https://www.claret.sbs",
+      "https://claret.claret.sbs",
     ],
     credentials: true,
   })
