74c59a334d37af6d9adacc059f7ff70d9bad92ee
Steven <steven@Stevens-MacBook-Air.local>
2026-05-13 23:03:46 +0800

    fix email bloodbank

--- src/utils/email.bloodbank.ts
+++ src/utils/email.bloodbank.ts
@@ -1,8 +1,9 @@
 import nodemailer from "nodemailer";
 
-// 🔐 Create transporter
-const transporter = nodemailer.createTransport({
-  service: "gmail",
+export const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
